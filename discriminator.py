20e1ca2b0540145ff25f69593154881af8b46224
kunchalasai670 <kunchalasai670@gmail.com>
2025-06-08 11:03:24 +0530

    Create discriminator.py

---
+++ discriminator.py
@@ -0,0 +1,14 @@
+import torch.nn as nn
+
+class Discriminator(nn.Module):
+    def __init__(self):
+        super(Discriminator, self).__init__()
+        self.model = nn.Sequential(
+            nn.Linear(1, 32),
+            nn.ReLU(),
+            nn.Linear(32, 1),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        return self.model(x)
